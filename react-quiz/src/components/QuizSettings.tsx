8f2aff7ae8ccffc7270c194842322762a85f56e6
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-07-12 12:05:51 +0200

    Updated options

--- react-quiz/src/components/QuizSettings.tsx
+++ react-quiz/src/components/QuizSettings.tsx
@@ -38,7 +38,6 @@ const QuizSettings: React.FC<QuizSettingsProps> = ({ quizState, selectedState, s
               value={selectedState.nrOfQuestions}
               onChange={handleSelectChange}
             >
-              <option value="5">5</option>
               <option value="10">10</option>
               <option value="15">15</option>
               <option value="20">20</option>
@@ -51,6 +50,7 @@ const QuizSettings: React.FC<QuizSettingsProps> = ({ quizState, selectedState, s
               value={selectedState.questionType}
                 onChange={handleSelectChange}
             >
+              <option value="">Any type</option>
               <option value="multiple">Multiple</option>
               <option value="boolean">True or false</option>
             </select>
@@ -62,6 +62,7 @@ const QuizSettings: React.FC<QuizSettingsProps> = ({ quizState, selectedState, s
               value={selectedState.difficulty}
               onChange={handleSelectChange}
             >
+              <option value="">Any difficulty</option>
               <option value="easy">Easy</option>
               <option value="medium">Medium</option>
               <option value="hard">Hard</option>
@@ -74,19 +75,20 @@ const QuizSettings: React.FC<QuizSettingsProps> = ({ quizState, selectedState, s
               value={selectedState.category}
               onChange={handleSelectChange}
             >
+              <option value="">Any category</option>
               <option value="9">General knowledge</option>
               <option value="17">Science and Nature</option>
               <option value="18">Computers</option>
               <option value="12">Music</option>
               <option value="10">Books</option>
               <option value="11">Movies</option>
+              <option value="26">Celebrities</option>
               <option value="14">TV</option>
-              <option value="15">Video Games</option>
-              <option value="19">Mathematics</option>
               <option value="21">Sports</option>
+              <option value="15">Video Games</option>
               <option value="23">History</option>
               <option value="25">Art</option>
-              <option value="26">Celebrities</option>
+              <option value="19">Mathematics</option>
               <option value="22">Geography</option>
               <option value="24">Politics</option>
             </select>
