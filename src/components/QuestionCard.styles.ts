194194f65e3b2ea09e74d0f57c1cdae3c6e3ac1d
Ivona Josipovic <josipovic.ivona@gmail.com>
2023-08-18 09:52:36 +0200

    Height for buttons updated to min-height

--- src/components/QuestionCard.styles.ts
+++ src/components/QuestionCard.styles.ts
@@ -35,7 +35,7 @@ export const ButtonWrapper = styled.div<ButtonWrapperProps>`
     user-select: none;
     font-size: 1rem;
     width: 100%;
-    height: 40px;
+    min-height: 40px;
     margin: 5px 0;
     background: ${({ correct, userClicked }) =>
       correct
